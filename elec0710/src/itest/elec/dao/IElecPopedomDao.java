1319efadb8419d4bf953a95c5a53b45ae02a8d24
chujian3yi <qlc111101@163.com>
2017-03-11 22:48:47 +0800

    0

--- elec0710/src/itest/elec/dao/IElecPopedomDao.java
+++
@@ -1,9 +0,0 @@
-package itest.elec.dao;
-
-import itest.elec.domain.ElecPopedom;
-
-public interface IElecPopedomDao extends ICommonDao<ElecPopedom>{
-	
-	public static final String SERVICE_NAME = "itest.elec.dao.impl.ElecPopedomDaoImpl";
-}
-
